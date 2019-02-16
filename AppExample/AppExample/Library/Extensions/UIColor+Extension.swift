dcb0280387d641593a5ea949dda03cd076aac0a2
Luis Fernando Mata Licón <fernando@fernandomata.mx>
2019-02-16 19:17:07 +0800

    Created UI helper classes and style guide. Added app fonts.

---
+++ AppExample/AppExample/Library/Extensions/UIColor+Extension.swift
@@ -0,0 +1,31 @@
+//
+//  UIColor+Extension.swift
+//  AppExample
+//
+//  Created by Luis Fernando Mata on 2/16/19.
+//  Copyright © 2019 Fernando. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// Taken from https://stackoverflow.com/a/27203691/2493073
+extension UIColor {
+    convenience init(hex:String) {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        self.init(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
