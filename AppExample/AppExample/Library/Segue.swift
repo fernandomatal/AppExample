54e117f24429f2da14dc69c113b9a8c3292a538a
Luis Fernando Mata Licón <fernando@fernandomata.mx>
2019-02-19 23:17:56 +0800

    Create segue helper enum.

---
+++ AppExample/AppExample/Library/Segue.swift
@@ -0,0 +1,13 @@
+//
+//  Segue.swift
+//  AppExample
+//
+//  Created by Luis Fernando Mata on 2/19/19.
+//  Copyright © 2019 Fernando. All rights reserved.
+//
+
+import Foundation
+
+enum Segue: String {
+    case showExperience = "ShowExperience"
+}
