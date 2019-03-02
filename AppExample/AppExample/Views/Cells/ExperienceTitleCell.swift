43cf8ce8d9fe1502db6f8dc6b5e4e2424da78823
Luis Fernando Mata Licón <fernando@fernandomata.mx>
2019-03-02 17:35:19 +0800

    Update experience title cell UI to show a list instead of a grid

--- AppExample/AppExample/Views/Cells/ExperienceTitleCell.swift
+++ AppExample/AppExample/Views/Cells/ExperienceTitleCell.swift
@@ -10,18 +10,12 @@ import UIKit
 
 class ExperienceTitleCell: UITableViewCell {
     
-    private var mainStackView: UIStackView!
-    
-    private var roleContentView: UIView!
     private var roleIcon: UIImageView!
     private var roleLabel: UILabel!
     
-    private var detailsStackView: UIStackView!
-    private var locationContentView: UIView!
     private var locationIcon: UIImageView!
     private var locationLabel: UILabel!
     
-    private var timeContentView: UIView!
     private var timeIcon: UIImageView!
     private var timeLabel: UILabel!
     
@@ -38,33 +32,83 @@ class ExperienceTitleCell: UITableViewCell {
     private func initMethods() {
         createUI()
         bindStyles()
+        selectionStyle = .none
     }
     
     private func createUI() {
-        roleContentView = UIView()
-        
-        roleIcon = UIImageView()
-        roleContentView.addSubview(roleIcon)
+        roleIcon = UIImageView(image: UIImage(named: "Briefcase"))
+        roleIcon.contentMode = .scaleAspectFit
+        contentView.addSubview(roleIcon)
         roleIcon.snp.makeConstraints { (make) in
-            make.top.left.bottom.equalToSuperview()
-            make.size.equalTo(20)
+            make.left.equalToSuperview().inset(16)
+            make.top.equalToSuperview().inset(24)
+            make.size.equalTo(25)
         }
         
         roleLabel = UILabel()
-        roleContentView.addSubview(roleLabel)
+        contentView.addSubview(roleLabel)
         roleLabel.snp.makeConstraints { (make) in
             make.centerY.equalTo(roleIcon)
-            make.right.equalToSuperview()
-            make.left.equalTo(roleIcon.snp.right)
+            make.right.equalToSuperview().inset(24)
+            make.left.equalTo(roleIcon.snp.right).offset(8)
+        }
+        
+        locationIcon = UIImageView(image: UIImage(named: "MapPin"))
+        locationIcon.contentMode = .scaleAspectFit
+        contentView.addSubview(locationIcon)
+        locationIcon.snp.makeConstraints { (make) in
+            make.centerX.equalTo(roleIcon)
+            make.top.equalTo(roleIcon.snp.bottom).offset(18)
+            make.size.equalTo(15)
+        }
+        
+        locationLabel = UILabel()
+        contentView.addSubview(locationLabel)
+        locationLabel.snp.makeConstraints { (make) in
+            make.centerY.equalTo(locationIcon)
+            make.left.right.equalTo(roleLabel)
+        }
+        
+        timeIcon = UIImageView(image: UIImage(named: "Calendar"))
+        timeIcon.contentMode = .scaleAspectFit
+        contentView.addSubview(timeIcon)
+        timeIcon.snp.makeConstraints { (make) in
+            make.centerX.equalTo(roleIcon)
+            make.top.equalTo(locationIcon.snp.bottom).offset(12)
+            make.size.equalTo(15)
+            make.bottom.equalToSuperview().inset(24).priority(800)
+        }
+        
+        timeLabel = UILabel()
+        contentView.addSubview(timeLabel)
+        timeLabel.snp.makeConstraints { (make) in
+            make.centerY.equalTo(timeIcon)
+            make.left.right.equalTo(roleLabel)
         }
     }
     
     private func bindStyles() {
         roleIcon.tintColor = UIColor.aeDarkerGrey
+        roleLabel.font = UIFont.Raleway.bold.withSize(28)
+        locationIcon.tintColor = UIColor.aeFlatGrey
+        locationLabel.configureWith(font: UIFont.Raleway.regular.withSize(16), color: UIColor.aeFlatGrey)
+        timeIcon.tintColor = UIColor.aeFlatGrey
+        timeLabel.configureWith(font: UIFont.Raleway.regular.withSize(16), color: UIColor.aeFlatGrey)
     }
     
-    func configureWith(iconName: String, title: String?, details: String?) {
-        
+    func configureWith(experience: Experience) {
+        roleLabel.text = experience.role
+        locationLabel.text = experience.location
+        timeLabel.text = calculateTime(joinedAt: experience.joinedAt, leftAt: experience.leftAt)
         layoutIfNeeded()
+    }
+    
+    private func calculateTime(joinedAt: Date, leftAt: Date?) -> String? {
+        let finalDate = leftAt ?? Date()
+        let formatter = DateComponentsFormatter()
+        formatter.maximumUnitCount = 2
+        formatter.unitsStyle = .full
+        formatter.allowedUnits = [.year, .month]
+        return formatter.string(from: joinedAt, to: finalDate)
     }
 }
